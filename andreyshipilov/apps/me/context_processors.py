3d43a819cafc70c94ec5094466f7ca7f9e43bddc
tezro <a@andreyshipilov.com>
2014-04-27 07:12:55 +0930

    dynamici age

--- andreyshipilov/apps/me/context_processors.py
+++ andreyshipilov/apps/me/context_processors.py
@@ -1,10 +1,15 @@
+from datetime import date
 from models import Social
 
 
 def socials(request):
+    now, birth = date.today(), date(1984, 1, 19)
+    age = (now.year - birth.year) - \
+        int((now.month, now.day) < (birth.month, birth.day))
     socials = Social.objects.all()
 
     return {
+        'AGE': age,
         'SOCIALS': socials,
         'SOCIALS_SPACER_PERCENTAGE': int(round(100 / (socials.count() - 1))),
     }
